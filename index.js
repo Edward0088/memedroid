c37dc3d49b82490c22502d7cd9f73c0b796b472e
Mehdi <user@example.com>
2025-09-16 10:49:43 +0330

    Initial Express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware برای JSON
+app.use(express.json());
+
+// Route نمونه
+app.get('/', (req, res) => {
+  res.send('Hello, World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
